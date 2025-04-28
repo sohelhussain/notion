22561f93c9e9c1f86f217e5590ea32bff7557de1
sohelhussain <sohelhussaing@gmail.com>
2025-04-28 09:16:57 +0530

    feat: GIT CHECK

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,7 @@ import Footer from "./_components/footer";
 export default function Home() {
   return (
     <div className="min-h-full flex flex-col">
+      
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
       <Heading />
       <Heroes />
